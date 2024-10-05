a99c83851eb5f7744fdfd9d7b025bba6ee554959
aryawaran4 <aryawaranggana.susilo@gmail.com>
2024-10-06 00:13:31 +0700

    feat: install prettier tailiwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        twinkle: {
+          '0%, 100%': { opacity: '1' },
+          '50%': { opacity: '0' },
+        },
+      },
+      animation: {
+        twinkle: 'twinkle 2s infinite ease-in-out',
+        twinkle2: 'twinkle 3s infinite ease-in-out',
+        twinkle3: 'twinkle 4s infinite ease-in-out',
+      },
+    },
   },
   plugins: [],
 }
