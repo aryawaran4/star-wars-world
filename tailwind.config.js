e7d4a2ee820e3ad0f2396b65d1964bf9ba6147bf
aryawaran4 <aryawaranggana.susilo@gmail.com>
2024-10-06 01:05:53 +0700

    feat: update homepage hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,46 @@ module.exports = {
   ],
   theme: {
     extend: {
-      keyframes: {
-        twinkle: {
-          '0%, 100%': { opacity: '1' },
-          '50%': { opacity: '0' },
-        },
-      },
       animation: {
-        twinkle: 'twinkle 2s infinite ease-in-out',
-        twinkle2: 'twinkle 3s infinite ease-in-out',
-        twinkle3: 'twinkle 4s infinite ease-in-out',
+        'twinkle-1': 'twinkle1 5s infinite',
+        'twinkle-2': 'twinkle2 7s infinite',
+        'twinkle-3': 'twinkle3 6s infinite',
+        'twinkle-4': 'twinkle4 8s infinite',
+        'twinkle-5': 'twinkle5 9s infinite',
+        flip: "flip 6s infinite steps(2, end)",
+        rotate: "rotate 3s linear infinite both",
+      },
+      keyframes: {
+        twinkle1: {
+          '0%, 100%': { transform: 'scale(1)', opacity: '1' },
+          '50%': { transform: 'scale(0.5)', opacity: '0.5' },
+        },
+        twinkle2: {
+          '0%, 100%': { transform: 'scale(1)', opacity: '1' },
+          '20%': { transform: 'scale(0.5)', opacity: '0.5' },
+        },
+        twinkle3: {
+          '0%, 100%': { transform: 'scale(1)', opacity: '1' },
+          '30%': { transform: 'scale(0.3)', opacity: '0.3' },
+        },
+        twinkle4: {
+          '0%, 100%': { transform: 'scale(1)', opacity: '1' },
+          '40%': { transform: 'scale(0.4)', opacity: '0.4' },
+        },
+        twinkle5: {
+          '0%, 100%': { transform: 'scale(1)', opacity: '1' },
+          '60%': { transform: 'scale(0.7)', opacity: '0.7' },
+        },
+        flip: {
+          to: {
+            transform: "rotate(360deg)",
+          },
+        },
+        rotate: {
+          to: {
+            transform: "rotate(90deg)",
+          },
+        },
       },
     },
   },
