40fbf9eac23c4d1686de74405f65e70a26c067aa
aryawaran4 <aryawaranggana.susilo@gmail.com>
2024-10-06 00:05:03 +0700

    feat: setting up route and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { Error404Component } from './pages/error-404/error-404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/homepage',
+  },
+  {
+    path: 'homepage',
+    component: HomepageComponent,
+    loadChildren: () => import('./pages/homepage/homepage.module').then(m => m.HomepageModule)
+  },
+  {
+    path: '**',
+    component: Error404Component,
+    loadChildren: () => import('./pages/error-404/error-404.module').then(m => m.Error404Module)
+  },
+];
